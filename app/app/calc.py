b39ea5c0dfb894f012917de181004f6e8a6d3af5
nabeelmehmood <nabeel_mehmood@hotmail.com>
2020-02-18 18:01:59 +0500

    added basic user model functionality

--- app/app/calc.py
+++
@@ -1,9 +0,0 @@
-
-def add(x, y):
-    """Add 2 numbers together"""
-    return x + y
-
-
-def subtract(x, y):
-    """Subtract 2 numbers"""
-    return x - y
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating new user with email works correctly"""
+        email = 'test@abc.com'
+        password = 'TestPass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password, password)
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is correctly normalized"""
+        email = "test@FereFERferf.com"
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_creating_user_with_invalid_email(self):
+        """Test that a new user creation fails if email is not provided"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
