602c8ee15462e5a51628f3fcd8e13c8aedfc6214
nabeelmehmood <nabeel_mehmood@hotmail.com>
2020-02-18 17:17:28 +0500

    linting flake 8 failing

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,4 +1,9 @@
 
-def add(x, y): 
+def add(x, y):
     """Add 2 numbers together"""
     return x + y
+
+def subtract(x, y):
+    """Subtract 2 numbers"""
+    return x - y
+    
--- app/app/test.py
+++ app/app/test.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, subtract
 
 
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test 2 numbers are added correctly"""
         self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test 2 numbers are subtracted correctly"""
+        self.assertEqual(subtract(11, 5), 6)
+        
