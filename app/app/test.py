58820ea1610fc487bcf3724db0b02c2542357b67
nabeelmehmood <nabeel_mehmood@hotmail.com>
2020-02-18 17:04:25 +0500

    fix directory structure causing build failure

--- app/app/test.py
+++ app/app/test.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 
 from app.calc import add
 
+
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test 2 numbers are added correctly"""
-        self.assertEqual(add(3,8), 11)
+        self.assertEqual(add(3, 8), 11)
